74f6f9132100a08c9c154fca7d554aeca5fc64fa
Artur Pragacz <arturpragacz@gmail.com>
2019-11-19 12:36:36 +0100

    initial commit

---
+++ fibo.h
@@ -0,0 +1,14 @@
+#ifndef INC_3_FIBO_H
+#define INC_3_FIBO_H
+
+#include <boost/dynamic_bitset.hpp>
+
+class Fibo {
+public:
+
+private:
+	boost::dynamic_bitset<> data;
+	void normalize();
+};
+
+#endif //INC_3_FIBO_H
