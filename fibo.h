de9922aa26d25d7ad383e34227156210ca6f0d66
Jagoda Kamińska <jagodamkaminska@gmail.com>
2019-11-19 18:02:31 +0100

    Work in progress: constructors

--- fibo.h
+++ fibo.h
@@ -2,13 +2,22 @@
 #define INC_3_FIBO_H
 
 #include <boost/dynamic_bitset.hpp>
+#include <string>
 
 class Fibo {
 public:
+    Fibo();
+    Fibo(const std::string&);
+    Fibo(const char*);
+    Fibo(int);
+    Fibo(const Fibo&);
 
 private:
 	boost::dynamic_bitset<> data;
 	void normalize();
 };
+
+Fibo Zero();
+Fibo One();
 
 #endif //INC_3_FIBO_H
